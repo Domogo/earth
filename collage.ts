c9436d6603ae7715e34cee0728328994819f11d7
Domogo <domsatrapa@gmail.com>
2025-01-21 00:10:01 +0100

    single earth

--- collage.ts
+++ collage.ts
@@ -3,14 +3,15 @@ import sharp from 'sharp';
 
 async function main() {
   try {
-    const gridImagePath = 'img/2025-01-19/grid/output.png';
+    // const gridImagePath = 'img/2025-01-19/grid/output.png';
+    const gridImagePath = 'img/2025-01-19/20250119190500.png'
     const gridImage = await sharp(gridImagePath);
     const reversedImageBuffer = await (await cut_and_reverse(gridImage, 16)).toBuffer();
     const reversedImage = sharp(reversedImageBuffer);
     const rotatedImage = await reversedImage.rotate(90).png();
     const finalImage = await cut_and_reverse(rotatedImage, 16);
     console.log('Final image created');
-    await finalImage.toFile('collage.png');
+    await finalImage.toFile('collage_single.png');
     console.log('Collage created successfully!');
   } catch (error) {
     console.error('Error creating collage:', error);
