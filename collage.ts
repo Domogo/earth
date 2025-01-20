b809f8689da41e465251784861d3dcbc38a613d7
Domogo <domsatrapa@gmail.com>
2025-01-20 23:24:00 +0100

    use 16

--- collage.ts
+++ collage.ts
@@ -7,10 +7,11 @@ async function main() {
     const gridImagePath = 'img/2025-01-19/grid/output.png';
     const gridImage = await sharp(gridImagePath);
     const reversedImage = await cut_and_reverse(gridImage, 16);
+    console.log('Reversed image created');
     const rotatedImage = await rotateImage(reversedImage);
+    console.log('Rotated image created');
     const finalImage = await cut_and_reverse(rotatedImage, 16);
-
-    console.log("final")
+    console.log('Final image created');
     await finalImage.png().toFile('collage.png');
     console.log('Collage created successfully!');
   } catch (error) {
