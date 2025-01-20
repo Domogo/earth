194379608303de4192072c91e9b9c0fd9e3c8d31
Domogo <domsatrapa@gmail.com>
2025-01-20 23:40:37 +0100

    collage

--- collage.ts
+++ collage.ts
@@ -1,19 +1,14 @@
 import { cut_and_reverse } from './cut_and_reverse.ts';
-import { rotateImage } from './rotate.ts';
 import sharp from 'sharp';
 
 async function main() {
   try {
     const gridImagePath = 'img/2025-01-19/grid/output.png';
     const gridImage = await sharp(gridImagePath);
-    const reversedImage = await cut_and_reverse(gridImage, 16);
-    await reversedImage.toFile('collage_1.png');
-    
-    reversedImage.rotate(90).png()
-    await reversedImage.toFile('collage_2.png');
-
-    console.log('Rotated image created');
-    const finalImage = await cut_and_reverse(reversedImage, 16);
+    const reversedImageBuffer = await (await cut_and_reverse(gridImage, 16)).toBuffer();
+    const reversedImage = sharp(reversedImageBuffer);
+    const rotatedImage = await reversedImage.rotate(90).png();
+    const finalImage = await cut_and_reverse(rotatedImage, 16);
     console.log('Final image created');
     await finalImage.toFile('collage.png');
     console.log('Collage created successfully!');
